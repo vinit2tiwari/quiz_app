12da73baaec965e4e67089e02e75abf51907c6ab
FlutLab.io <>
2022-06-28 06:53:54 +0000

    Initial commit

---
+++ android/app/src/main/kotlin/com/quiz/demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.quiz.demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mentor_quiz_app_tut/home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Quiz app', theme: ThemeData(primaryColor: const Color(0xFF55C1EF)), home: Home());
+  }
+}
