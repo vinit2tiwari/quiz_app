607099d66c0253d457f3c940001fc58cc6ebf60e
FlutLab.io <>
2022-06-29 13:34:03 +0000

    Autosave

--- lib/welcome.dart
+++ lib/welcome.dart
@@ -56,10 +56,7 @@ class WelcomeScreen extends StatelessWidget {
                     ),
                   ),
                   Spacer(flex: 2),
-                  Text(
-                    "Name: Anshika Anand \nRoll no: 102103380\nBranch:COE,Class:1COE-14",
-                    style: TextStyle(color: Colors.purple),
-                  ) // it will take 2/6 spaces
+                  Padding(padding: EdgeInsets.all(kDefaultPadding * 0.75), child: Text("Name: Anshika Anand \nRoll no: 102103380\nBranch:COE,Class:1COE-14", style: TextStyle(color: Colors.purple))) // it will take 2/6 spaces
                 ],
               ),
             ),
